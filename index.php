b890435abed55975a54c6ed0bbe13d36588bf37e
Athul Ramachandran <87645899+Ajabehak@users.noreply.github.com>
2025-08-11 12:13:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "thul!";
+echo "jjhiyuj7iujykhkty Streamline payment management with a unified dashboard, enabling both online and in-person payment collection while enhancing conversion rates and minimizing fraud.";
